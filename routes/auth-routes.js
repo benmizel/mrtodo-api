92ef43bf2b89e647670af54bb4d0dee8011e0f13
Ben Mizel <bemizel@gmail.com>
2025-01-22 17:10:24 -0500

    Fix minor issue with file saving on local repo.

---
+++ routes/auth-routes.js
@@ -0,0 +1,8 @@
+// import express from "express";
+// import * as authController from "../controllers/auth-controller.js";
+// const authRouter = express.Router();
+// inventoriesRouter.route("/signup").post(authController.signUp);
+// inventoriesRouter.route("/login").post(authController.login);
+// inventoriesRouter.route("/:id").delete(authController.deleteUser);
+
+// export default authRouter;
--- routes/tasks-routes.js
+++ routes/tasks-routes.js
@@ -1,13 +1,13 @@
-import express from "express";
-import * as tasksController from "../controllers/tasks-controller.js";
-const tasksRouter = express.Router();
-tasksRouter
-  .route("/")
-  .get(tasksRouter.getAllTasks)
-  .post(tasksRouter.createTask)
-tasksRouter
-  .route("/:id")
-  .put(tasksRouter.updateTask)
-  .delete(tasksRouter.deleteTask);
+// import express from "express";
+// import * as tasksController from "../controllers/tasks-controller.js";
+// const tasksRouter = express.Router();
+// tasksRouter
+//   .route("/")
+//   .get(tasksRouter.getAllTasks)
+//   .post(tasksRouter.createTask)
+// tasksRouter
+//   .route("/:id")
+//   .put(tasksRouter.updateTask)
+//   .delete(tasksRouter.deleteTask);
 
-export default tasksRouter;
+// export default tasksRouter;
