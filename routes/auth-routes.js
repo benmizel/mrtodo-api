2cc9ac14cfe31646bebabbed284015149aff4184
Ben Mizel <bemizel@gmail.com>
2025-02-11 23:22:59 -0500

    Comment out rate limiter due to test deployment issues.

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,18 +1,24 @@
 import express from "express";
 import * as authController from "../controllers/auth-controller.js";
-import {
-  loginLimiter,
-  refreshTokenLimiter,
-} from "../utils/rateLimiter-utils.js";
+// import {
+//   loginLimiter,
+//   refreshTokenLimiter,
+// } from "../utils/rateLimiter-utils.js";
 
 const authRouter = express.Router();
 
 authRouter.route("/signup").post(authController.signUp);
 
-authRouter.route("/login").post(loginLimiter, authController.login);
+// authRouter.route("/login").post(loginLimiter, authController.login);
+
+// authRouter
+//   .route("/refresh-token")
+//   .post(authController.refreshToken);
+
+authRouter.route("/login").post(authController.login);
 
 authRouter
   .route("/refresh-token")
-  .post(refreshTokenLimiter, authController.refreshToken);
+  .post(authController.refreshToken);
 
 export default authRouter;
